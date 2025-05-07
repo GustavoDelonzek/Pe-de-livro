2ecdf11ddf6fd75c8b77ad1b9bfb721715bedfed
GustavoDelonzek <gustavo2016delonzek@gmail.com>
2025-05-07 14:17:06 -0300

    feat: define the attributes for model Book and your relationships

--- backend/app/Models/Book.php
+++ backend/app/Models/Book.php
@@ -4,8 +4,43 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Book extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'author_id',
+        'publisher_id',
+        'published_year',
+        'price',
+        'stock',
+        'img_url'
+    ];
+
+    protected $hidden = [
+        'deleted_at'
+    ];
+
+    public function author(){
+        return $this->belongsTo(Author::class);
+    }
+
+    public function publisher(){
+        return $this->belongsTo(Publisher::class);
+    }
+
+    public function reviews(){
+        return $this->hasMany(Review::class);
+    }
+
+    public function genres(){
+        return $this->belongsToMany(Genre::class, 'book_genre', 'book_id', 'genre_id');
+    }
+
+    public function ordersItems(){
+        return $this->hasMany(OrderItem::class);
+    }
 }
