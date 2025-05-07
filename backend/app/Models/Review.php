3831e4e6cc60973eaa3e904fa54cb1d1509dd6c4
GustavoDelonzek <gustavo2016delonzek@gmail.com>
2025-05-07 16:49:18 -0300

    feat: define the attributes for model Review and your relationships

--- backend/app/Models/Review.php
+++ backend/app/Models/Review.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'book_id',
+        'user_id',
+        'rating',
+        'comment'
+    ];
+
+    protected $hidden = [
+        'deleted_at'
+    ];
+
+    public function book(){
+        return $this->belongsTo(Book::class);
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
 }
