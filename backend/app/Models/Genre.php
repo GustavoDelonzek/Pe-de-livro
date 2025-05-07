4ea6d2c27d79eb34d6b7b4ecb63b147b962d6e23
GustavoDelonzek <gustavo2016delonzek@gmail.com>
2025-05-07 13:58:11 -0300

    feat: define the attributes for model Genre and your relationships

--- backend/app/Models/Genre.php
+++ backend/app/Models/Genre.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Genre extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name'
+    ];
+
+    protected $hidden = [
+        'deleted_at'
+    ];
+
+    public function books(){
+        return $this->belongsToMany(Book::class, 'book_genre', 'genre_id', 'book_id');
+    }
+
+    public function user(){
+        return $this->belongsToMany(User::class, 'user_genre', 'genre_id', 'user_id');
+    }
 }
