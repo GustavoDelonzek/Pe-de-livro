0a7804a8cb90cfcb824264dc18da4702405907fc
GustavoDelonzek <gustavo2016delonzek@gmail.com>
2025-05-07 16:50:31 -0300

    feat: define the attributes for model Order and your relationships

--- backend/app/Models/Order.php
+++ backend/app/Models/Order.php
@@ -4,8 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Order extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'total',
+        'status'
+    ];
+
+    protected $hidden = [
+        'deleted_at'
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function items(){
+        return $this->hasMany(OrderItem::class);
+    }
+
+
 }
