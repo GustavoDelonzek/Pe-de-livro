8648528423b319dea5048a7d21615c3d4fe101da
GustavoDelonzek <gustavo2016delonzek@gmail.com>
2025-05-07 16:54:04 -0300

    feat: define the attributes for model OrderItem and your relationships

--- backend/app/Models/OrderItem.php
+++ backend/app/Models/OrderItem.php
@@ -4,8 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class OrderItem extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'order_id',
+        'book_id',
+        'quantity',
+        'unit_price'
+    ];
+
+    protected $hidden = [
+        'deleted_at'
+    ];
+
+    public function order(){
+        return $this->belongsTo(Order::class);
+    }
+
+    public function book(){
+        return $this->belongsTo(Book::class);
+    }
 }
