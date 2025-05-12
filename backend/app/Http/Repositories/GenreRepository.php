1863d9c7274eeac28df4ccd01701dfc38dcd3f1f
GustavoDelonzek <gustavo2016delonzek@gmail.com>
2025-05-12 02:19:56 -0300

    feat: create functions essentials for repository of genre

--- backend/app/Http/Repositories/GenreRepository.php
+++ backend/app/Http/Repositories/GenreRepository.php
@@ -6,6 +6,22 @@ use App\Models\Genre;
 
 class GenreRepository{
     public function getAll(){
-        return Genre::all();
+        return Genre::query();
+    }
+
+    public function createGenre(array $genre){
+        return Genre::create($genre);
+    }
+
+    public function getGenreById(int $id){
+        return Genre::where('id', $id);
+    }
+
+    public function updateGenre(array $genre, int $id){
+        return Genre::where('id', $id)->update($genre);
+    }
+
+    public function deleteGenre(int $id){
+        return Genre::where('id', $id)->delete();
     }
 }
