1289af770dc315385e0629c1666ae6f48513a1fb
GustavoDelonzek <gustavo2016delonzek@gmail.com>
2025-05-17 20:06:03 -0300

    feat: create functions necessary for repository of reviews

--- backend/app/Http/Repositories/ReviewRepository.php
+++ backend/app/Http/Repositories/ReviewRepository.php
@@ -6,6 +6,26 @@ use App\Models\Review;
 
 class ReviewRepository{
     public function getAll(){
-        return Review::all();
+        return Review::query();
+    }
+
+    public function createReview(array $review){
+        return Review::create($review);
+    }
+
+    public function getReviewById(int $id){
+        return Review::where('id', $id);
+    }
+
+    public function updateReview(array $review, int $id){
+        return Review::where('id', $id)->update($review);
+    }
+
+    public function deleteReview(int $id){
+        return Review::where('id', $id)->delete();
+    }
+
+    public function getReviewByBookId(int $bookId){
+        return Review::where('book_id', $bookId);
     }
 }
